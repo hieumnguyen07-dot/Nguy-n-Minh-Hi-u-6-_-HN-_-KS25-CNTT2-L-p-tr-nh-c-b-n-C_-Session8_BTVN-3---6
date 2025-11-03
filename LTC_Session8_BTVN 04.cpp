c53957c158d4d3d79e4d9b7e8feaedb26dc8650f
HIEU 6 <hieu.m.nguyen07@gmail.com>
2025-11-03 07:27:39 +0700

    Refactor array variable name in main function

--- LTC_Session8_BTVN 04.cpp
+++ LTC_Session8_BTVN 04.cpp
@@ -2,16 +2,17 @@
 
 
 int main(){
-	int arrayarranger[5];
+	int arr[5];
 	int i;
-	for (i = 0 ; i < sizeof(arrayarranger)/sizeof(int); i++){
+	for (i = 0 ; i < sizeof(arr)/sizeof(int); i++){
 		printf(" \n array[%d] = ", i);
-		scanf("%d",&arrayarranger[i]);
+		scanf("%d",&arr[i]);
 	}   printf("\n value of these array stuff");
-	for (i = 0 ;i < sizeof(arrayarranger)/sizeof(int); i++  ){
-		printf("array[%d] = %d \n ", i , arrayarranger[i]);
+	for (i = 0 ;i < sizeof(arr)/sizeof(int); i++  ){
+		printf("array[%d] = %d \n ", i , arr[i]);
 		
 	}
 	return 0;
 	
 }
+
